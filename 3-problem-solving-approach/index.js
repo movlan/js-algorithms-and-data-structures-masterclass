64c59acc8a3957e43ab596b6c06b381914be6b94
Bahram M <bmovlanov@gmail.com>
2024-03-17 22:03:21 -0400

    add

--- 3-problem-solving-approach/index.js
+++ 3-problem-solving-approach/index.js
@@ -6,20 +6,14 @@ function charCount(str) {
     // make char lowercase
     ch = ch.toLowerCase();
     // if char is a number or letter then
-    if (ch.match(/[a-z0-9]/i)) {
-      if (result[ch]) {
-        // if char in object, increment count
-        result[ch] += 1;
-      } else {
-        // if cher is not in the object, add it to the object and set value to 1
-        result[ch] = 1;
-      }
-      // if cher is something else (space, period, etc.) do not do anything
+    if (/[a-z0-9]/.test(ch)) {
+      result[ch] = ++result[ch] || 1;
     }
+    // if cher is something else (space, period, etc.) do not do anything
   }
   // return object
   return result;
 }
 
 console.log(charCount('hello'));
-console.log(charCount('My phone number is 555-555-5555'));
+console.log(charCount('My phonnnnne number is 555-555-5555'));
