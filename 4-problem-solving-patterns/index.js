7855d14c8f874bf079373dbf70644f92552e273e
Bahram Movlanov <bahram.movlanov@passportinc.com>
2024-03-25 13:56:25 -0400

    count unique values

--- 4-problem-solving-patterns/index.js
+++ 4-problem-solving-patterns/index.js
@@ -1,3 +1,25 @@
+function countUniqueValues(arr) {
+  if (Array.isArray(arr)) {
+    if (arr.length === 0) return 0
+    let i = 0;
+    let j = 1;
+  
+    while (j < arr.length) {
+      if (arr[i] !== arr[j]) {
+        i++;
+        arr[i] = arr[j]
+      }
+      j++;
+    }
+    return i+1;
+  }
+}
+
+console.log(countUniqueValues([1,1,1,1,1,2])) // 2
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) // 2
+console.log(countUniqueValues([])) // 2
+console.log(countUniqueValues([-2,-1,-1,0,1])) // 2
+
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -39,6 +61,6 @@ function anagram(str1, str2) {
   return true
 }
 
-console.log(anagram('', ''))
-console.log(anagram('anagram', 'nagara'))
-console.log(anagram('anagram', 'nagaram'))
+// console.log(anagram('', ''))
+// console.log(anagram('anagram', 'nagara'))
+// console.log(anagram('anagram', 'nagaram'))
